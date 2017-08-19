0767efd916129e4f79474d6fc0c40052e6d2c594
Emmanuel Joseph <jekayode@live.com>
2017-08-19 04:57:17 +0100

    Added pages

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php include 'views/header.html'; ?>
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+    <hr>
+
+<div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-3">
+			
+		<img src="jekayode-profile-img.jpg" alt="Emmanuel Joseph" class="img" width="200px">
+
+
+        </div>
+
+        <div class="col-md-8">
+        <h2>Emmaneul Joseph</h2>
+        <p>
+        	<i class="fa fa-slack" aria-hidden="true"></i> <a href="">@jekayode</a>   |  <i class="fa fa-github" aria-hidden="true"> </i> <a href="https://github.com/jekayode" target="_blank">@jekayode</a>  | <a href="https://github.com/jekayode/hnl-internship" target="_blank">#Stage1 Output</a>
+        </p>
+			
+		<p>
+        	My name is Emmanuel. I am comfortable with HTML, CSS, JS, PHP (and Laravel). I currently reside in Ilorin, Kwara State.
+        </p>
+
+        <p>
+        	This profile page was create as part of Stage 2 of the Hotels.ng Developer Internship Project. 
+        </p>
+
+
+        </div>
+        
+        </div>
+
+<?php include 'views/footer.html'; ?>
+</div>

d76174ed05aa41e8a9b9f4a21d61b8cc3e570cb5
Tim <fabrobocomx@gmail.com>
2017-08-31 19:57:59 +0100

    Conflict

--- coinsman/index.php
+++ coinsman/index.php
@@ -7,11 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Popular Cryptocurrencies</title>
-<<<<<<< HEAD
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/main.css">
-=======
->>>>>>> 5398b7ed2196c5945745d541095f100b18768bd4
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
